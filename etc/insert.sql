31b04047dcfe3485a1e9b04f0fab3774566bf9de
AmirFragman <128620781+AmirFragman@users.noreply.github.com>
2023-06-03 16:15:22 +0300

    Delete etc directory

--- etc/insert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Customers 
-VALUES 
-(1, "shimon", "tel aviv", 27, 1),
-(2, "amir", "tel aviv", 30, 1);
-
-INSERT INTO Books 
-VALUES
-(1, "The Count of Monte Cristo", "Alexandre Dumas", 1844, 1, 1),
-(2, "Lord of the rings", "JRR Tolkien", 1954, 2, 1);
